4c029483cd6326377770e97fd76b04e1e19a1cc6
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-06-01 12:47:04 +0200

    Test-Counter-Jest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function Increment() {
+    setCounter((preNumber) => preNumber + 1);
+  }
+  function Decrement() {
+    setCounter((preNumber) => preNumber - 1);
+  }
+  function Restart() {
+    setCounter((preNumber) => (preNumber = 0));
+  }
+  function Switch() {
+    setCounter((preNumber) => preNumber * -1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <header>{counter}</header>
+      <section>
+        <button onClick={Increment}>Increment</button>
+        <button onClick={Decrement}>Decrement</button>
+        <button onClick={Restart}>Restart</button>
+        <button onClick={Switch}>Switch Sign</button>
+      </section>
     </div>
   );
 }
